cb976cf5edd0335ad444be249bde32c700c56ff8
Mohosin999 <mohosin.h.akash@gmail.com>
2023-09-12 08:11:26 +0600

    configure database password

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "shoestore"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "MohosinH9923$"),
+      password: env("DATABASE_PASSWORD", "Akash123"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
     },
   },
