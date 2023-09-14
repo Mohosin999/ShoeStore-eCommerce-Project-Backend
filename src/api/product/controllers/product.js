c0487cd6f2ea51092114772c5efd280c03e63794
Mohosin999 <mohosin.h.akash@gmail.com>
2023-09-14 20:33:18 +0600

    remove commented codes from product.js

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -28,32 +28,3 @@ module.exports = createCoreController("api::product.product", ({ strapi }) => ({
     }
   },
 }));
-
-// "use strict";
-
-// /**
-//  * product controller
-//  */
-
-// const { createCoreController } = require("@strapi/strapi").factories;
-
-// module.exports = createCoreController("api::product.product", ({ strapi }) => ({
-//   // Hot discount function
-//   async hotDiscount(ctx) {
-//     try {
-//       ctx.query = { ...ctx.query, local: "en" };
-
-//       // Calling the default core action
-//       const { data } = await super.find(ctx);
-
-//       // Filter and show those data which hot_discount will be true
-//       const filteredData = data.filter(
-//         (item) => item.attributes.hot_discount === true
-//       );
-
-//       return { filteredData };
-//     } catch (err) {
-//       return ctx.send({ error: err.message });
-//     }
-//   },
-// }));
